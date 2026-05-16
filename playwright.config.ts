0612ccce2948cf26fab4262343f6697a0dd40435
nitinshinde9696 <nitinshinde9696@gmail.com>
2026-05-16 16:26:08 +0530

    initial setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,11 +23,12 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  timeout: 30 * 10000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
-
+    
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -36,6 +37,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
+      screenshot: 'only-on-failure',
       use: { ...devices['Desktop Chrome'] },
     },
 
--- tests/TestPractice1.spec.ts
+++ tests/TestPractice1.spec.ts
@@ -2,12 +2,12 @@ import { test } from '../Fixtures/loginfixture';
 import { readFileSync } from 'fs';
 
 test.describe('test suite', () => {
-    test('navigate and login to book my show',{tag:['@smoket']}, async ({loginPage, homePage}) => {
+    // test('navigate and login to book my show',{tag:['@smoket']}, async ({loginPage, homePage}) => {
 
-        await loginPage.login('practice','SuperSecretPassword!');
+    //     await loginPage.login('practice','SuperSecretPassword!');
         
-        await homePage.validateHomePage();
-    });
+    //     await homePage.validateHomePage();
+    // });
     // test('read Json file',async ({}) => {
     //     const data= await JSON.parse(readFileSync('./test-data/productsData.json', 'utf-8'));
     //     const productName=data.Car
@@ -17,7 +17,44 @@ test.describe('test suite', () => {
     //         console.log("product name: "+product.description);
     //     }
 //     }
+test('get second highest occurrence of a number in an array',async ({}) => {
+    const arr=[1,1,2,3,4,5,6,7,8,9,10,1,2,3,4,1,4,5,6,7,8,9];
+    const countMap=new Map();  
+    const uniqueNumbers=new Set(arr);
+    console.log("unique numbers: "+Array.from(uniqueNumbers));
+    for(const num of arr){
+        if(countMap.has(num)){
+            countMap.set(num, countMap.get(num)+1);
+            
+        }else{
+            countMap.set(num, 1);
+        }   
+    }
+    for(const [key,value] of countMap.entries()){
+        console.log("number : "+key+" count: "+value);
+    }
 
-// );
+    const sortedCount=Array.from(countMap.entries()).sort((a,b)=>b[1]-a[1]);
+    console.log("second highest occurrence: "+sortedCount[1][0]);
+    for(const [key,value] of sortedCount){
+        console.log("number : "+key+" count: "+value);
+    }
+}
+);
+test('permutation of a string',async ({}) => {
+    const str="abc";    
+    const permute=(s:string, prefix:string="")=>{
+        if(s.length===0){
+            console.log(prefix);
+        }   
+        for(let i=0;i<s.length;i++){
+            const rem=s.substring(0,i)+s.substring(i+1);
+            permute(rem, prefix+s[i]);
+        }
+    }
+    permute(str);
 
+
+
+});
 });
